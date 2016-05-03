7b1dbdafe8b6fee94f5b7e51eb2b2f9ca0f51d43
claire <claire.mariko.smith@gmail.com>
2016-05-03 22:46:27 +0100

    landing xml

--- app/src/main/java/example/codeclan/com/eightball/Landing.java
+++ app/src/main/java/example/codeclan/com/eightball/Landing.java
@@ -1,6 +1,11 @@
 package example.codeclan.com.eightball;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
 
 /**
  * Created by user on 03/05/2016.
@@ -8,4 +13,29 @@ import android.support.v7.app.AppCompatActivity;
 public class Landing extends AppCompatActivity {
 
 
+    ImageButton mYodaButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        mYodaButton = (ImageButton) findViewById(R.id.yoda_button);
+
+        mYodaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Landing:", "submit button clicked!");
+
+                Intent ClickedButton = new Intent(Landing.this, AnswerDetails.class);
+
+                startActivity(ClickedButton);
+            }
+
+
+        });
+
+    }
 }
