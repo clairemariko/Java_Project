9ea91de173039208aae2b2cc67359891a971f2c9
claire <claire.mariko.smith@gmail.com>
2016-05-05 06:18:37 +0100

    set background to null

--- app/src/main/java/example/codeclan/com/eightball/Landing.java
+++ app/src/main/java/example/codeclan/com/eightball/Landing.java
@@ -1,16 +1,18 @@
 package example.codeclan.com.eightball;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.content.res.Resources;
 
 /**
  * Created by user on 03/05/2016.
  */
-public class Landing extends AppCompatActivity {
+public class Landing extends Activity {
 
 
     ImageButton mStarWarsButton;
